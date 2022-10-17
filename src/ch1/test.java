c1137481acc28c86849d16f004c47737fb8016cb
komkof <komkof@gmail.com>
2022-10-17 20:21:13 +0400

    some changes

--- src/ch1/test.java
+++ src/ch1/test.java
@@ -1,4 +1,24 @@
 package ch1;
 
 public class test {
+    public static void main(String[] args) {
+        //Integer.toBinaryString(N)
+//        int N = 7; // 5=101 6=110 7=111 8=1000
+//        String s = "";
+//        for (int n = N; n > 0; n /= 2)
+//            s = (n % 2) + s;
+//        System.out.println(f(N,""));
+        System.out.println(1 % 2);
+        System.out.println(1 % 3);
+        System.out.println(3 % 4);
+        System.out.println(11 % 14);
+    }
+
+    static String f(int a, String result) {
+        int remainder = a % 2;
+        int quotient = a / 2;
+        if (quotient > 1) return f(quotient, remainder + result);
+        else return quotient + "" + remainder + result;
+    }
+
 }
