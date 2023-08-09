a3bbd36e0f8359682ba6d871bd18a2b7ed85b746
komkof_linux <komkof@gmail.com>
2023-08-09 11:59:28 +0400

    215 from Linux my solution small change

--- src/ch1/WiggleSortII324.java
+++ src/ch1/WiggleSortII324.java
@@ -1,26 +1,30 @@
 package ch1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class WiggleSortII324 {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 5, 1, 1, 6, 4};
-        new WiggleSortII324().wiggleSort(arr, 4);
+        int[] arr = new int[]{12, 5, 16, 1, 7, 55, 27};
+        new WiggleSortII324().wiggleSort(arr, 7);
         System.out.println(Arrays.toString(arr));
-        arr = new int[]{1, 3, 2, 2, 3, 1};
-        new WiggleSortII324().wiggleSort(arr, 3);
-        System.out.println(Arrays.toString(arr));
-        arr = new int[]{4, 5, 5, 6};
-        new WiggleSortII324().wiggleSort(arr, 5);
-        System.out.println(Arrays.toString(arr));
-        arr = new int[]{1, 4, 3, 4, 1, 2, 1, 3, 1, 3, 2, 3, 3};
-        new WiggleSortII324().wiggleSort(arr, 3);
-        System.out.println(Arrays.toString(arr));
+//        arr = new int[]{1, 3, 2, 2, 3, 1};
+//        new WiggleSortII324().wiggleSort(arr, 3);
+//        System.out.println(Arrays.toString(arr));
+//        arr = new int[]{4, 5, 5, 6};
+//        new WiggleSortII324().wiggleSort(arr, 5);
+//        System.out.println(Arrays.toString(arr));
+//        arr = new int[]{1, 4, 3, 4, 1, 2, 1, 3, 1, 3, 2, 3, 3};
+//        new WiggleSortII324().wiggleSort(arr, 3);
+//        System.out.println(Arrays.toString(arr));
     }
 
     public void wiggleSort(int[] nums, int mid) {
-        dutchFlag(nums, mid);
+        //dutchFlag(nums, mid);
+        //quickSelect(new int[]{12, 5, 16, 1, 7, 55, 27}, 4);
     }
 
     public void dutchFlag(int[] nums, int mid) {
@@ -45,5 +49,34 @@ public class WiggleSortII324 {
         int t = a[i];
         a[i] = a[j];
         a[j] = t;
+    }
+
+    public int quickSelect(List<Integer> nums, int k) {
+        int pivotIndex = new Random().nextInt(nums.size());
+        int pivot = nums.get(pivotIndex);
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        for (int num: nums) {
+            if (num > pivot) {
+                left.add(num);
+            } else if (num < pivot) {
+                right.add(num);
+            } else {
+                mid.add(num);
+            }
+        }
+
+        if (k <= left.size()) {
+            return quickSelect(left, k);
+        }
+
+        if (left.size() + mid.size() < k) {
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+
+        return pivot;
     }
 }
