cd7381d43a406a10fe81b210f276e3f917a13891
komkof_linux <komkof@gmail.com>
2023-08-29 11:31:56 +0400

    215 from Linux my failed try with PriorityQueue

--- src/ch1/Test.java
+++ src/ch1/Test.java
@@ -11,23 +11,20 @@ import java.util.stream.Collectors;
 public class Test {
 
     public static void main(String[] args) {
-//        System.out.println(Arrays.toString(new Test().dailyTemperatures(new int[]{73, 72, 71, 70, 69, 68, 69, 70, 71, 72, 73, 74, 75, 75, 75, 74, 73, 76})));
-//        System.out.println(Arrays.toString(new Test().dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
-//        System.out.println(Arrays.toString(new Test().dailyTemperatures(new int[]{30, 40, 50, 60})));
-//        System.out.println(Arrays.toString(new Test().dailyTemperatures(new int[]{30, 60, 90})));
+
 //        System.out.println(new Test().removeKdigits("1432219", 3));
 //        System.out.println(new Test().removeKdigits("12312999", 3));
-//        System.out.println(new Test().removeKdigits("177799", 2));
+        System.out.println(new Test().removeKdigits("177799", 2));
 //        System.out.println(new Test().removeKdigits("1432219", 3));
 //        System.out.println(new Test().removeKdigits("10200", 1));
 //        System.out.println(new Test().removeKdigits("", 1));
-        long time = System.currentTimeMillis();
-        try {
-            System.out.println(new Test().removeKdigits(Files.readString(Path.of("src/ch1/stringtoolong.txt")), 50000));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(System.currentTimeMillis() - time);
+//        long time = System.currentTimeMillis();
+//        try {
+//            System.out.println(new Test().removeKdigits(Files.readString(Path.of("src/ch1/stringtoolong.txt")), 50000));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(System.currentTimeMillis() - time);
     }
 
     public String removeKdigits(String num, int k) {
@@ -37,19 +34,31 @@ public class Test {
         int lenOfChoise = k + 1;
         String result = "";
         int min = 0;
-        for (int i = 0; i < lenOfResult; i++) {
-            String minStr = "z";
-            for (int j = min; j < lenOfChoise + i; j++) {
-                if (numList.get(j).compareTo(minStr) < 0) {
-                    min = j;
-                    minStr = numList.get(j);
+        PriorityQueue<Pair> queue = new PriorityQueue<>((p1, p2) -> p1.value.compareTo(p2.value) > 0 ? 1: -11);
+        if (lenOfResult > 0) {
+            for (int i = 0; i < lenOfChoise; i++) queue.add(new Pair(numList.get(i), i));
+            for (int i = 0; i < lenOfResult; i++) {
+                Pair minPair = queue.poll();
+                while (minPair.index < min) {
+                    minPair = queue.poll();
                 }
+                min = minPair.index;
+                if (i < lenOfResult - 1) queue.add(new Pair(numList.get(lenOfChoise + i), lenOfChoise + i));
+                result += minPair.value;
             }
-            result += minStr;
-            min++;
         }
         System.out.println(System.currentTimeMillis() - time);
         return result.isEmpty() ? "0" : result.replaceFirst("^0+(?!$)", "");
+    }
+
+    public class Pair {
+        String value;
+        int index;
+
+        public Pair(String value, int index) {
+            this.value = value;
+            this.index = index;
+        }
     }
 
     public int[] dailyTemperatures(int[] temperatures) {
